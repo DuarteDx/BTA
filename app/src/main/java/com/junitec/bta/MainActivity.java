fbc9becb0b3dbb9e4ca3d34372d9822019dfdf6b
Duarte Dias <DuartexDias@gmail.com>
2017-06-26 22:30:11 +0200

    + - buttons change level from 0 to 3

--- app/src/main/java/com/junitec/bta/MainActivity.java
+++ app/src/main/java/com/junitec/bta/MainActivity.java
@@ -1,5 +1,7 @@
 package com.junitec.bta;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,27 +12,44 @@ import static android.R.attr.level;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final int[] level = {0};
 
-        /*final TextView level_textview = (TextView) findViewById(R.id.level_textview);
+        final TextView level_textview = (TextView) findViewById(R.id.level_textview);
         Button plus = (Button) findViewById(R.id.plus_button);
         Button minus = (Button) findViewById(R.id.minus_button);
 
-        level_textview.setText(level);
+        level_textview.setText(""+ level[0]);
 
         plus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                level_textview.setText("1");
+                if(level[0] <3) {
+                    level[0]++;
+                    level_textview.setText(""+ level[0]);
+                }
             }
         });
 
         minus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                level_textview.setText("3");
+                if(level[0] >0) {
+                    level[0]--;
+                    level_textview.setText(""+ level[0]);
+                }
             }
-        });*/
+        });
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+
+        
     }
 }
